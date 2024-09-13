faebc2ac29e0e389a7eeea1fcf8d5ca129a866df
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-09-13 11:25:58 +0200

    change header

--- src/shared/HeaderComponent.vue
+++ src/shared/HeaderComponent.vue
@@ -13,50 +13,49 @@ export default {
 </script>
 
 <template>
-  <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand logo" href="#"
-          ><img src="../../public/boolbnb.svg" alt=""
-        /></a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link :to="{ name: 'home' }" class="mr-15 link"
-                >Home</router-link
-              >
-            </li>
-            <li class="nav-item">
-              <router-link :to="{ name: 'contact' }" class="link"
-                >Contact</router-link
-              >
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+  <header class="shadow">
+    <div class="container-lg">
+      <div class="row">
+        <div class="col d-none d-lg-flex">
+          <a
+            class="navbar-brand d-flex justify-content-start align-items-start"
+            href="#"
+            ><img class="logo" src="../../public/boolbnb.svg" alt=""
+          /></a>
+        </div>
+        <div class="col">
+          <div class="justify-content-center d-none d-lg-flex">
+            <ul class="nav-menu d-flex mx-auto">
+              <li class="nav-item">
+                <router-link :to="{ name: 'home' }" class="mr-15 link"
+                  >Home</router-link
+                >
+              </li>
+              <li class="nav-item">
+                <router-link :to="{ name: 'contact' }" class="link"
+                  >Contact</router-link
+                >
+              </li>
+            </ul>
+          </div>
+          <div class="search-bar">
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Recipient's username"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <span class="input-group-text" id="basic-addon2">%</span>
+            </div>
+          </div>
+        </div>
+        <div class="col d-flex justify-content-end align-items-start m-2">
+          <button class="btn-login">Accedi</button>
         </div>
       </div>
-    </nav>
+    </div>
   </header>
 </template>
 
@@ -68,10 +67,8 @@ header {
   font-size: 20px;
   position: sticky;
   top: 0;
+  background-color: whitesmoke;
 
-  #app {
-    height: 150px;
-  }
   .logo {
     width: 150px;
   }
@@ -79,33 +76,29 @@ header {
   ul {
     padding: 0;
     margin: 10px;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  li {
-    list-style-type: none;
-  }
-
-  svg {
-    margin-left: -10px;
-    filter: drop-shadow(5px 5px 5px rgba(0, 0, 0, 0.2));
-    transition: 0.3s;
-
-    &:hover {
-      transform: scale(1.05);
+    li {
+      list-style-type: none;
     }
   }
 
   a {
     color: $tertiary;
+    text-decoration: none;
 
     &:hover {
       color: $primary;
 
       font-weight: 600;
+    }
+  }
+  .btn-login {
+    border: 1px solid $primary;
+    color: $primary;
+    padding: 5px 10px;
+    border-radius: 20px;
+    &:hover {
+      background-color: $primary;
+      color: white;
     }
   }
 }
