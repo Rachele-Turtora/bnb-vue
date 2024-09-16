5712daf5cd8d8a45cb58b53b8f8468f4b7f61c72
byHYONS <byhyons@gmail.com>
2024-09-16 20:28:39 +0200

    feat: aggiunta pagina 404

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -7,13 +7,58 @@
 
 <template>
     <div>
-      <h1>Appartamento non trovato</h1>
-      <p>Non siamo riusciti a trovare l'appartamento che stai cercando.</p>
-      <router-link :to="{name: 'details',params: { slug: apartment.slug } }" class="mr-15 link"></router-link>
+        <div class="not-found">
+            <div class="container">
+                <div class="image">
+                    <img src="/public/img/404.svg" alt="404 page">
+                    <div class="text">
+                        <h3>Torna indietro:</h3>
+                        <router-link :to="{name: 'home' }" class="link-404">Indietro</router-link>
+    
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
   </template>
   
   
-  <style scoped>
+  <style lang="scss" scoped>
+  
+  @use '../assets/scss/partials/extende' as *;
+ 
+
+
+  .not-found {
+    overflow: auto;
+    width: 100vw;
+    height: calc(100vh - 120px);
+    
+    .container {
+        .image {
+            margin: 30px auto 20px;
+            width: 80%;
+        }
+    }
+    .text {
+        margin-left: 30px;
+        margin-top: 60px;
+        .link-404 {
+            margin-top: 15px;
+            display: inline-block;
+            padding: 10px 70px;
+            background: $gradient;
+            color: $text2;
+            font-size: 20px;
+            border-radius: 10px;
+            @extend %shadow;
+            &:hover {
+                opacity: .85;
+                @extend %shadow2;
+            }
+        }
+    }
+  }
 
   </style>
