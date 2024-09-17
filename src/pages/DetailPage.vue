1be0a9f94a9d1a24c8d37d372ce896a131c1e97e
Rachele-Turtora <racheleturtora21@gmail.com>
2024-09-17 12:07:45 +0200

    [UPD] servizi in DetailPage

--- src/pages/DetailPage.vue
+++ src/pages/DetailPage.vue
@@ -2,46 +2,57 @@
 import axios from 'axios';
 import { store } from '../store';
 import MapComponent from '../components/MapComponent.vue';
-    export default {
-        name: 'DetailPage',
-        data() {
-            return {
-                slug: [],
-                apartment: [],
-                store,
-            }
-        },
-        components: {
-            MapComponent
-        },
-        methods: {
-            getApiDetail(){
-                axios
+export default {
+    name: 'DetailPage',
+    data() {
+        return {
+            slug: [],
+            apartment: [],
+            services: {
+                "WiFi": "wifi",
+                "Posto macchina": "car",
+                "Piscina": "person-swimming",
+                "Portineria": "person-shelter",
+                "Sauna": "temperature-arrow-up",
+                "Vista Mare": "water",
+                "TV": "tv",
+                "Ascensore": "elevator",
+                "Aria condizionata": "fan",
+                "Kit medico": "kit-medical"
+            },
+            store,
+        }
+    },
+    components: {
+        MapComponent
+    },
+    methods: {
+        getApiDetail() {
+            axios
                 .get(`${this.store.api.baseUrl}homes/${this.$route.params.slug}`)
                 .then((response) => {
-                    console.log(response);
-                    
+
                     if (response.data.status) {
                         this.apartment = response.data.results;
-                                               
-                    } else {
-                        this.$route.push({name: 'not-found'});
-                    }
-                    
-                })
-            }
 
-        },
-        mounted() {
-            this.getApiDetail();
-            
-        },
-        computed: {
-            goBack() {
-            this.$router.push({ name: 'home'});
-            }
+                    } else {
+                        this.$route.push({ name: 'not-found' });
+                    }
+
+                })
+        }
+
+    },
+    mounted() {
+        this.getApiDetail();
+
+    },
+    computed: {
+        goBack() {
+            this.$router.push({ name: 'home' });
         }
     }
+}
 </script>
 
 
@@ -51,35 +62,45 @@ import MapComponent from '../components/MapComponent.vue';
             <div class="button mb-5">
                 <button @click="goBack">Torna Indietro</button>
             </div>
-        <div class="item_card" @click="openShow">
-            <div class="card_detail row">
-                <div class="card_image col-12 col-md-5 align-content-center">
-                    <img
-                    :src="apartment.image && apartment.image.startsWith('http') ? apartment.image : apartment.image_frontend"
-                    :alt="apartment.title"/>
+            <div class="item_card" @click="openShow">
+                <div class="card_detail row">
+                    <h2 class="mb-5">{{ apartment.title }}</h2>
+                    <div class="card_image col-12 col-md-5 align-content-center">
+                        <img :src="apartment.image && apartment.image.startsWith('http') ? apartment.image : apartment.image_frontend"
+                            :alt="apartment.title" />
+                    </div>
+                    <div class="card_info col-12 col-md-7 mt-5 ps-5">
+                        <p class="mt-4"><span>Indirizzo: </span> {{ apartment.address }}</p>
+                        <p class="mt-4"><span>Camere: </span> {{ apartment.rooms }}</p>
+                        <p class="mt-4"><span>Letti: </span> {{ apartment.beds }}</p>
+                        <p class="mt-4"><span>Bagni: </span> {{ apartment.bathrooms }}</p>
+                    </div>
                 </div>
-                <div class="card_info col-12 col-md-7 mt-5 ps-5">   
-                    <p class="mt-4"><span>Titolo: </span> {{ apartment.title }}</p>
-                    <p class="mt-4"><span>Indirizzo: </span> {{ apartment.address }}</p>
-                    <p class="mt-4"><span>Camere: </span> {{ apartment.rooms }}</p>
-                    <p class="mt-4"><span>Letti: </span> {{ apartment.beds }}</p>
-                    <p class="mt-4"><span>Bagni: </span> {{ apartment.bathrooms }}</p>                  
+                <div class="card_desscription my-5">
+                    <p v-if="apartment.description"><span>Descrizione: </span> {{ apartment.description }} </p>
                 </div>
-            </div>
-            <div class="card_desscription my-5">
-                <p v-if="apartment.description"><span>Descrizione: </span> {{ apartment.description }} </p>
-            </div>
-            <div class="map mt-3">
-                <MapComponent class="mt-5" :apartment="apartment"/>
+
+                <div v-if="apartment.services?.length" class="services">
+                    <h3>Cosa troverai:</h3>
+                    <ul>
+                        <li v-for="service in apartment.services" class="d-flex mt-3" :key="service.name">
+                            <!-- Usa direttamente l'icona -->
+                            <font-awesome-icon v-if="services[service.name]" :icon="['fas', services[service.name]]"
+                                class="me-3 fs-5" />
+                            <p>{{ service.name }}</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="map mt-3">
+                    <MapComponent class="mt-5" :apartment="apartment" />
+                </div>
             </div>
         </div>
-    </div>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
-
 @use '../assets/scss/partials/extende' as *;
 
 .detail {
@@ -114,39 +135,35 @@ import MapComponent from '../components/MapComponent.vue';
             overflow: scroll;
             @extend %shadow;
             cursor: pointer;
-           
+
             span {
                 color: $primary;
                 font-weight: 600;
 
-                
+
                 .card_detail {
                     align-items: center;
 
-                   
+
                     .card_image {
                         height: 400px;
                         align-content: center;
                         object-fit: cover;
-            
+
                         img {
                             height: 100%;
                         }
-                    } 
-                    .card_info {
-                        
-                    
-                    }  
+                    }
+
+                    .card_info {}
                 }
-                .card_desscription {
-                   
-                }
-            }   
-            
+
+                .card_desscription {}
+            }
+
         }
     }
 }
-
 </style>
 
 
