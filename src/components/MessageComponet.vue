73b06f28465fc3280615f3532e3054d803d23d11
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-09-17 16:34:04 +0200

    [upd] messagecomponet css (da migliorare)

--- src/components/MessageComponet.vue
+++ src/components/MessageComponet.vue
@@ -18,6 +18,7 @@ export default {
                 content: '',
             },
             messageSuccess: '',
+            messageError: ''
 
         }
     },
@@ -64,7 +65,7 @@ export default {
 </script>
 
 <template>
-    <div class="message-component">
+    <!-- <div class="message-component">
         <div class="container">
             <div class="form-message">
                 <form @submit.prevent="sendMessage">
@@ -84,7 +85,7 @@ export default {
                  </form>
             </div>
             <div class="info-message">
-                <!-- Mostra il messaggio di successo o errore -->
+                
                 <div v-if="messageSuccess" class="success">
                 {{ messageSuccess }}
                 </div>
@@ -94,7 +95,33 @@ export default {
             </div>
         </div>
 
-    </div>
+    </div> -->
+    <form action="#" @submit.prevent="sendMessage">
+       <div id="form">
+        <h1>Compila il form sottostante per inviarmi un messaggio</h1>
+        <label for="name">Nome:</label>
+        <input type="text" name="name" v-model="form.name" id="name" required/>
+
+        <label for="email">Email:</label>
+        <input type="text" name="email" v-model="form.email" id="email" required />
+
+        <label for="message">Messaggio</label>
+        <textarea name="message" v-model="form.content" id="message" required cols="30" rows="10"></textarea>
+        
+        <!-- <input type="submit" id="submit" name="submit" value="Invia" /> -->
+            <button type="submit">Invia</button>
+          
+       </div>
+    </form>
+            <div class="info-message">
+                
+                <div v-if="messageSuccess" class="success">
+                {{ messageSuccess }}
+                </div>
+                <div v-if="messageError" class="error">
+                {{ messageError }}
+                </div>
+            </div>
 </template>
 
 <style lang="scss" scoped>
@@ -102,10 +129,63 @@ export default {
 @use '../assets/scss/partials/extende' as *;
 
 
-.message-component {
 
-
-
+#form {
+  font-size: 14px;
+  max-width: 500px;
+  margin: 50px auto;
+  padding: 24px;
+  overflow: hidden;
+  -moz-border-radius: 20px;
+  -webkit-border-radius: 20px;
+  border-radius: 20px;
+  border: 1px solid black;
+  @extend %shadow;
+}
+h1 {
+ font-size: 16px;
+ color: black;
+ margin-bottom: 20px;
 }
 
+label, input, textarea { /* Stili comuni agli elementi del form */
+color: black; /* Colore del testo */
+float: left; /* Float a sinistra */
+font-family: Verdana, sans-serif; /* Tipo di carattere per il testo */
+margin: 10px 0; /* Margini */
+}
+label { /* Stili per la label */
+display: block; /* Impostiamo la label come elemento blocco */
+line-height: 30px; /* Altezza di riga */
+width: 150px; /* Larghezza */
+}
+input, textarea { /* Stili per il campo di testo e per la textarea */
+//  background: #1C1C1C; /* Colore di sfondo */
+//  border: 1px solid $primary; /* Bordo */
+ color: black; /* Colore del testo */
+ height: 30px; /* Altezza */
+ line-height: 30px; /* Altezza di riga */
+ width: 300px; /* Larghezza */
+ padding: 0 10px; /* Padding */
+}
+input { padding-left: 30px;}
+textarea { /* Stili specifici per la teaxtarea */
+ /* Sfondo con immagine */
+ font-size: 12px;
+ height: 200px;
+ width: 300px;
+ overflow: hidden; /* disabilitare la scrollbar in IE */
+}
+button {
+  margin-top: 8px;
+  margin-left: 20px;
+  border: 1px solid black;
+  background-color: transparent;
+  padding: 4px 8px;
+  color: $footer;
+
+}
+button:active {
+  transform: scale(0.95);
+}
 </style>
