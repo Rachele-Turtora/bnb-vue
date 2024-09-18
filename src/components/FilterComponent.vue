7565509cad0b9a3ef3f46327e35a5216f209b996
Rachele-Turtora <racheleturtora21@gmail.com>
2024-09-18 12:00:19 +0200

    [UPD] getServices axios

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,112 +1,48 @@
 <script>
 import { store } from "../store";
+import axios from 'axios';
 export default {
   name: "FilterComponent",
 
   data() {
     return {
       store,
+      services: [],
+      selectedServices: [],
       filters: {
-        service1: false,
-        service2: false,
-        service3: false, //piscina id 3
-        service4: false, //portineria id 4
-        service5: false, //sauna id 5
-        service6: false, //vistamare id 6
         numrooms: 0,
         numtoilets: 0,
         numbeds: 0,
-        rangeDistance: 10, 
+        rangeDistance: 10,
       },
-      filterResultApartments: [],
     };
   },
   methods: {
+    getServices() {
+      const url = this.store.api.baseUrl + 'services';
+
+      axios.get(url).then(response => {
+        this.services = response.data.results;
+        console.log(this.services);
+      })
+    },
     filterResult() {
-      this.filterResultApartments = store.api.filteredApartments;
+      this.store.api.filteredApartments = store.api.filteredApartments;
 
-      this.filterResultApartments = this.filterResultApartments.filter((apartment) =>
-        apartment.distance <= this.filters.rangeDistance
-      );
-      if (
-        this.filters.service1 ||
-        this.filters.service2 ||
-        this.filters.service3 ||
-        this.filters.service4 ||
-        this.filters.service5 ||
-        this.filters.service6
-      ) {
-        if (this.filters.service1) {
-          console.log("filtro wifi attivo");
-          this.filterResultApartments = this.filterResultApartments.filter(
-            (appartment) => {
-              return appartment.services.some(
-                (service) => (service.pivot.service_id === 1)
-              );
-            }
-          );
-        }
-        if (this.filters.service2) {
-          console.log("filtro auto attivo");
+      if (this.selectedServices.length > 0) {
+        console.log("Filtraggio per servizi attivi");
 
-          this.filterResultApartments = this.filterResultApartments.filter(
-            (appartment) => {
-              return appartment.services.some(
-                (service) => (service.pivot.service_id === 2)
-              );
-            }
-          );
-        }
-        if (this.filters.service3) {
-          console.log("filtro piscina attivo");
-
-          this.filterResultApartments = this.filterResultApartments.filter(
-            (appartment) => {
-              return appartment.services.some(
-                (service) => (service.pivot.service_id === 3)
-              );
-            }
-          );
-        }
-        if (this.filters.service4) {
-          console.log("filtro porta attivo");
-
-          this.filterResultApartments = this.filterResultApartments.filter(
-            (appartment) => {
-              return appartment.services.some(
-                (service) => (service.pivot.service_id === 4)
-              );
-            }
-          );
-        }
-        if (this.filters.service5) {
-          console.log("filtro sauna attivo");
-
-          this.filterResultApartments = this.filterResultApartments.filter(
-            (appartment) => {
-              return appartment.services.some(
-                (service) => (service.pivot.service_id === 5)
-              );
-            }
-          );
-        }
-        if (this.filters.service6) {
-          console.log("filtro vista mare attivo");
-
-          this.filterResultApartments = this.filterResultApartments.filter(
-            (appartment) => {
-              return appartment.services.some(
-                (service) => (service.pivot.service_id === 6)
-              );
-            }
-          );
-        }
+        this.store.api.filteredApartments = this.store.api.filteredApartments.filter(appartment =>
+          this.selectedServices.every(serviceId =>
+            appartment.services.some(service => service.pivot.service_id === serviceId)
+          )
+        );
       }
 
       if (this.filters.numrooms > 0) {
         console.log("filtro camere attivo");
 
-        this.filterResultApartments = this.filterResultApartments.filter(
+        this.store.api.filteredApartments = this.store.api.filteredApartments.filter(
           (appartment) => (appartment.rooms === this.filters.numrooms)
         );
       }
@@ -114,7 +50,7 @@ export default {
       if (this.filters.numbeds > 0) {
         console.log("filtro letti attivo");
 
-        this.filterResultApartments = this.filterResultApartments.filter(
+        this.store.api.filteredApartments = this.store.api.filteredApartments.filter(
           (appartment) => (appartment.beds === this.filters.numbeds)
         );
       }
@@ -122,24 +58,23 @@ export default {
       if (this.filters.numtoilets > 0) {
         console.log("filtro bagni attivo");
 
-        this.filterResultApartments = this.filterResultApartments.filter(
+        this.store.api.filteredApartments = this.store.api.filteredApartments.filter(
           (appartment) => (appartment.bathrooms === this.filters.numtoilets)
         );
       }
 
-      console.log(this.filterResultApartments);
+      console.log(this.store.api.filteredApartments);
+      this.$emit('filter-apartments', store.api.filteredApartments);
     },
   },
+  created() {
+    this.getServices();
+  }
 };
 </script>
 <template>
   <!-- Button trigger modal -->
-  <button
-    type="button"
-    class="btn btn-success"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-  >
+  <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
     <span>
       <font-awesome-icon :icon="['fas', 'bars-staggered']" class="fs-4" />
     </span>
@@ -147,145 +82,50 @@ export default {
   </button>
 
   <!-- Modal -->
-  <div
-    class="modal fade"
-    id="exampleModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">
             Filtra la ricerca
           </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <form @submit.prevent="filterResult">
           <div class="modal-body">
-            <label for="customRange3" class="form-label"
-              >Cerca nel raggio di:</label
-            >
-            <input
-              type="range"
-              class="form-range"
-              min="10"
-              max="80"
-              step="10"
-              id="range-distance"
-              v-model="filters.rangeDistance"
-            />
+            <label for="customRange3" class="form-label">Cerca nel raggio di:</label>
+            <input type="range" class="form-range" min="10" max="80" step="10" id="range-distance" />
 
-            <div
-              class="service-section my-4 d-flex justify-content-evenly flex-wrap"
-            >
-              <input
-                type="checkbox"
-                class="btn-check"
-                id="service-1"
-                v-model="this.filters.service1"
-                autocomplete="off"
-              />
-              <label class="btn btn-outline-primary" for="service-1"
-                >Wi-fi</label
-              ><br />
-              <input
-                type="checkbox"
-                class="btn-check"
-                id="service-2"
-                v-model="this.filters.service2"
-                autocomplete="off"
-              />
-              <label class="btn btn-outline-primary" for="service-2"
-                >Posto auto</label
-              ><br />
-              <input
-                type="checkbox"
-                class="btn-check"
-                id="service-3"
-                v-model="this.filters.service3"
-                autocomplete="off"
-              />
-              <label class="btn btn-outline-primary" for="service-3"
-                >Piscina </label
-              ><br />
-              <input
-                type="checkbox"
-                class="btn-check"
-                id="service-4"
-                v-model="this.filters.service4"
-                autocomplete="off"
-              />
-              <label class="btn btn-outline-primary" for="service-4"
-                >Portineria</label
-              ><br />
-              <input
-                type="checkbox"
-                class="btn-check"
-                id="service-5"
-                v-model="this.filters.service5"
-                autocomplete="off"
-              />
-              <label class="btn btn-outline-primary" for="service-5"
-                >Sauna</label
-              ><br />
-              <input
-                type="checkbox"
-                class="btn-check"
-                id="service-6"
-                v-model="this.filters.service6"
-                autocomplete="off"
-              />
-              <label class="btn btn-outline-primary" for="service-6"
-                >Vista mare</label
-              ><br />
+            <div class="service-section my-4 d-flex justify-content-evenly flex-wrap">
+              <div class="service" v-for="service in services" :key="service.id">
+                <input type="checkbox" class="btn-check" :id="'service-' + service.id" v-model="this.selectedServices"
+                  :value="service.id" autocomplete="off" />
+                <label class="btn btn-outline-primary" :for="'service-' + service.id">
+                  {{ service.name }}
+                </label>
+              </div>
             </div>
 
             <div class="room-section">
               <div class="row my-2">
                 <h4 class="w-50">Numero di camere</h4>
-                <input
-                  class="input-filter w-25"
-                  type="number"
-                  name="rooms"
-                  id="rooms-numb"
-                  v-model="this.filters.numrooms"
-                />
+                <input class="input-filter w-25" type="number" name="rooms" id="rooms-numb"
+                  v-model="this.filters.numrooms" />
               </div>
               <div class="row my-2">
                 <h4 class="w-50">Numero di letti</h4>
-                <input
-                  class="input-filter w-25"
-                  type="number"
-                  name="beds"
-                  id="beds-numb"
-                  v-model="this.filters.numbeds"
-                />
+                <input class="input-filter w-25" type="number" name="beds" id="beds-numb"
+                  v-model="this.filters.numbeds" />
               </div>
               <div class="row my-2">
                 <h4 class="w-50">Numero di bagni</h4>
-                <input
-                  class="input-filter w-25"
-                  type="number"
-                  name="toilet"
-                  id="toilet-numb"
-                  v-model="this.filters.numtoilets"
-                />
+                <input class="input-filter w-25" type="number" name="toilet" id="toilet-numb"
+                  v-model="this.filters.numtoilets" />
               </div>
             </div>
           </div>
           <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-danger"
-              data-bs-dismiss="modal"
-            >
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
               Close
             </button>
             <button type="submit" class="btn btn-success">Filtra</button>
--- src/pages/ResultsPage.vue
+++ src/pages/ResultsPage.vue
@@ -8,66 +8,68 @@ import GoogleMapFrame from "../components/GoogleMapFrame.vue";
 import FilterComponent from "../components/FilterComponent.vue";
 
 export default {
-  nome: "Results",
-  props: {},
-  data() {
-    return {
-      store,
-    };
-  },
-  components: {
-    GoogleMap,
-    CardsComponent,
-    FilterComponent,
-  },
+   nome: "Results",
+   props: {},
+   data() {
+      return {
+         store,
+      };
+   },
+   components: {
+      GoogleMap,
+      CardsComponent,
+      FilterComponent,
+   },
 
-  computed: {
-    apartmentsNum() {
-      return this.store.api.filteredApartments.length;
-    },
-  },
+   computed: {
+      apartmentsNum() {
+         return this.store.api.filteredApartments.length;
+      },
+   },
+   methods: {
+      updateFilteredApartments(apartments) {
+         this.store.api.filteredApartments = apartments;
+      }
+   }
 };
 </script>
 
 <template>
-  <div class="container mt-4">
-    <div class="d-flex justify-content-between align-items-center mx-3">
-      <p>
-        <strong>{{ apartmentsNum }} alloggi in questa località</strong>
-      </p>
-      <div class="filters my-3 mx-2">
-        <FilterComponent></FilterComponent>
+   <div class="container mt-4">
+      <div class="d-flex justify-content-between align-items-center mx-3">
+         <p>
+            <strong>{{ apartmentsNum }} alloggi in questa località</strong>
+         </p>
+         <div class="filters my-3 mx-2">
+            <FilterComponent @filter-apartments="updateFilteredApartments" />
+         </div>
       </div>
-    </div>
 
-    <div class="row">
-      <div
-        class="col-lg-3 col-md-4 col-sm-6"
-        v-for="(apartment, index) in store.api.filteredApartments"
-        :key="apartment.id"
-      >
-        <CardsComponent :apartment="apartment" />
+      <div class="row">
+         <div class="col-lg-3 col-md-4 col-sm-6" v-for="(apartment, index) in store.api.filteredApartments"
+            :key="apartment.id">
+            <CardsComponent :apartment="apartment" />
+         </div>
       </div>
-    </div>
-  </div>
+   </div>
 </template>
 
 <style lang="scss" scoped>
 img {
-  height: 100%;
+   height: 100%;
 }
 
 .img-container {
-  height: 180px;
+   height: 180px;
 
-  &:hover {
-    transform: scale(1.01);
-    transition: 0.3s;
-    cursor: pointer;
-  }
+   &:hover {
+      transform: scale(1.01);
+      transition: 0.3s;
+      cursor: pointer;
+   }
 }
 
 .filters {
-  width: 120px;
+   width: 120px;
 }
 </style>
