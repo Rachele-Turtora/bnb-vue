c6c856fa55f0a962d620480211fe337fee78f2c3
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-09-18 12:10:38 +0200

    [UPD] FIX MODAL CLOSE

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,6 +1,6 @@
 <script>
 import { store } from "../store";
-import axios from 'axios';
+import axios from "axios";
 export default {
   name: "FilterComponent",
 
@@ -19,12 +19,12 @@ export default {
   },
   methods: {
     getServices() {
-      const url = this.store.api.baseUrl + 'services';
+      const url = this.store.api.baseUrl + "services";
 
-      axios.get(url).then(response => {
+      axios.get(url).then((response) => {
         this.services = response.data.results;
         console.log(this.services);
-      })
+      });
     },
     filterResult() {
       this.store.api.filteredApartments = store.api.filteredApartments;
@@ -32,49 +32,60 @@ export default {
       if (this.selectedServices.length > 0) {
         console.log("Filtraggio per servizi attivi");
 
-        this.store.api.filteredApartments = this.store.api.filteredApartments.filter(appartment =>
-          this.selectedServices.every(serviceId =>
-            appartment.services.some(service => service.pivot.service_id === serviceId)
-          )
-        );
+        this.store.api.filteredApartments =
+          this.store.api.filteredApartments.filter((appartment) =>
+            this.selectedServices.every((serviceId) =>
+              appartment.services.some(
+                (service) => service.pivot.service_id === serviceId
+              )
+            )
+          );
       }
 
       if (this.filters.numrooms > 0) {
         console.log("filtro camere attivo");
 
-        this.store.api.filteredApartments = this.store.api.filteredApartments.filter(
-          (appartment) => (appartment.rooms === this.filters.numrooms)
-        );
+        this.store.api.filteredApartments =
+          this.store.api.filteredApartments.filter(
+            (appartment) => appartment.rooms === this.filters.numrooms
+          );
       }
 
       if (this.filters.numbeds > 0) {
         console.log("filtro letti attivo");
 
-        this.store.api.filteredApartments = this.store.api.filteredApartments.filter(
-          (appartment) => (appartment.beds === this.filters.numbeds)
-        );
+        this.store.api.filteredApartments =
+          this.store.api.filteredApartments.filter(
+            (appartment) => appartment.beds === this.filters.numbeds
+          );
       }
 
       if (this.filters.numtoilets > 0) {
         console.log("filtro bagni attivo");
 
-        this.store.api.filteredApartments = this.store.api.filteredApartments.filter(
-          (appartment) => (appartment.bathrooms === this.filters.numtoilets)
-        );
+        this.store.api.filteredApartments =
+          this.store.api.filteredApartments.filter(
+            (appartment) => appartment.bathrooms === this.filters.numtoilets
+          );
       }
 
       console.log(this.store.api.filteredApartments);
-      this.$emit('filter-apartments', store.api.filteredApartments);
+      this.$emit("filter-apartments", store.api.filteredApartments);
     },
   },
   created() {
     this.getServices();
-  }
+  },
 };
 </script>
 <template>
   <!-- Button trigger modal -->
-  <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  <button
+    type="button"
+    class="btn btn-success"
+    data-bs-toggle="modal"
+    data-bs-target="#exampleModal"
+  >
     <span>
       <font-awesome-icon :icon="['fas', 'bars-staggered']" class="fs-4" />
     </span>
@@ -82,25 +93,60 @@ export default {
   </button>
 
   <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">
             Filtra la ricerca
           </h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <form @submit.prevent="filterResult">
           <div class="modal-body">
-            <label for="customRange3" class="form-label">Cerca nel raggio di:</label>
-            <input type="range" class="form-range" min="10" max="80" step="10" id="range-distance" />
+            <label for="customRange3" class="form-label"
+              >Cerca nel raggio di:</label
+            >
+            <input
+              type="range"
+              class="form-range"
+              min="10"
+              max="80"
+              step="10"
+              id="range-distance"
+            />
 
-            <div class="service-section my-4 d-flex justify-content-evenly flex-wrap">
-              <div class="service" v-for="service in services" :key="service.id">
-                <input type="checkbox" class="btn-check" :id="'service-' + service.id" v-model="this.selectedServices"
-                  :value="service.id" autocomplete="off" />
-                <label class="btn btn-outline-primary" :for="'service-' + service.id">
+            <div
+              class="service-section my-4 d-flex justify-content-evenly flex-wrap"
+            >
+              <div
+                class="service"
+                v-for="service in services"
+                :key="service.id"
+              >
+                <input
+                  type="checkbox"
+                  class="btn-check"
+                  :id="'service-' + service.id"
+                  v-model="this.selectedServices"
+                  :value="service.id"
+                  autocomplete="off"
+                />
+                <label
+                  class="btn btn-outline-primary"
+                  :for="'service-' + service.id"
+                >
                   {{ service.name }}
                 </label>
               </div>
@@ -109,26 +155,51 @@ export default {
             <div class="room-section">
               <div class="row my-2">
                 <h4 class="w-50">Numero di camere</h4>
-                <input class="input-filter w-25" type="number" name="rooms" id="rooms-numb"
-                  v-model="this.filters.numrooms" />
+                <input
+                  class="input-filter w-25"
+                  type="number"
+                  name="rooms"
+                  id="rooms-numb"
+                  v-model="this.filters.numrooms"
+                />
               </div>
               <div class="row my-2">
                 <h4 class="w-50">Numero di letti</h4>
-                <input class="input-filter w-25" type="number" name="beds" id="beds-numb"
-                  v-model="this.filters.numbeds" />
+                <input
+                  class="input-filter w-25"
+                  type="number"
+                  name="beds"
+                  id="beds-numb"
+                  v-model="this.filters.numbeds"
+                />
               </div>
               <div class="row my-2">
                 <h4 class="w-50">Numero di bagni</h4>
-                <input class="input-filter w-25" type="number" name="toilet" id="toilet-numb"
-                  v-model="this.filters.numtoilets" />
+                <input
+                  class="input-filter w-25"
+                  type="number"
+                  name="toilet"
+                  id="toilet-numb"
+                  v-model="this.filters.numtoilets"
+                />
               </div>
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+            <button
+              type="button"
+              class="btn btn-danger"
+              data-bs-dismiss="modal"
+            >
               Close
             </button>
-            <button type="submit" class="btn btn-success">Filtra</button>
+            <button
+              type="submit"
+              class="btn btn-success"
+              data-bs-dismiss="modal"
+            >
+              Filtra
+            </button>
           </div>
         </form>
       </div>
