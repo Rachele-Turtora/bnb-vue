92070fb7b59b68dffbf5a5f5829d85c6f9abf4b8
Rachele-Turtora <racheleturtora21@gmail.com>
2024-09-18 14:21:36 +0200

    [UPD] button filters

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -88,74 +88,36 @@ export default {
 </script>
 <template>
   <!-- Button trigger modal -->
-  <button
-    type="button"
-    class="btn btn-success"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-  >
-    <span>
-      <font-awesome-icon :icon="['fas', 'bars-staggered']" class="fs-4" />
-    </span>
-    <p class="ms-3 mb-0 fs-6">Filtri</p>
+  <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <div class="d-flex align-items-center">
+      <span>
+        <font-awesome-icon :icon="['fas', 'bars-staggered']" class="fs-4" />
+      </span>
+      <p class="ms-3 mb-0 fs-6">Filtri</p>
+    </div>
   </button>
 
   <!-- Modal -->
-  <div
-    class="modal fade"
-    id="exampleModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">
             Filtra la ricerca
           </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <form @submit.prevent="filterResult">
           <div class="modal-body">
-            <label for="customRange3" class="form-label"
-              >Cerca nel raggio di:</label
-            >
-            <input
-              type="range"
-              class="form-range"
-              min="10"
-              max="80"
-              step="10"
-              id="range-distance"
-              v-model="filters.rangeDistance"
-            />
+            <label for="customRange3" class="form-label">Cerca nel raggio di:</label>
+            <input type="range" class="form-range" min="10" max="80" step="10" id="range-distance"
+              v-model="filters.rangeDistance" />
 
-            <div
-              class="service-section my-4 d-flex justify-content-evenly flex-wrap"
-            >
-              <div
-                class="service"
-                v-for="service in services"
-                :key="service.id"
-              >
-                <input
-                  type="checkbox"
-                  class="btn-check"
-                  :id="'service-' + service.id"
-                  v-model="this.selectedServices"
-                  :value="service.id"
-                  autocomplete="off"
-                />
-                <label
-                  class="btn btn-outline-primary"
-                  :for="'service-' + service.id"
-                >
+            <div class="service-section my-4 d-flex justify-content-evenly flex-wrap">
+              <div class="service" v-for="service in services" :key="service.id">
+                <input type="checkbox" class="btn-check" :id="'service-' + service.id" v-model="this.selectedServices"
+                  :value="service.id" autocomplete="off" />
+                <label class="btn btn-outline-primary" :for="'service-' + service.id">
                   {{ service.name }}
                 </label>
               </div>
@@ -164,49 +126,26 @@ export default {
             <div class="room-section">
               <div class="row my-2">
                 <h4 class="w-50">Numero di camere</h4>
-                <input
-                  class="input-filter w-25"
-                  type="number"
-                  name="rooms"
-                  id="rooms-numb"
-                  v-model="this.filters.numrooms"
-                />
+                <input class="input-filter w-25" type="number" name="rooms" id="rooms-numb"
+                  v-model="this.filters.numrooms" />
               </div>
               <div class="row my-2">
                 <h4 class="w-50">Numero di letti</h4>
-                <input
-                  class="input-filter w-25"
-                  type="number"
-                  name="beds"
-                  id="beds-numb"
-                  v-model="this.filters.numbeds"
-                />
+                <input class="input-filter w-25" type="number" name="beds" id="beds-numb"
+                  v-model="this.filters.numbeds" />
               </div>
               <div class="row my-2">
                 <h4 class="w-50">Numero di bagni</h4>
-                <input
-                  class="input-filter w-25"
-                  type="number"
-                  name="toilet"
-                  id="toilet-numb"
-                  v-model="this.filters.numtoilets"
-                />
+                <input class="input-filter w-25" type="number" name="toilet" id="toilet-numb"
+                  v-model="this.filters.numtoilets" />
               </div>
             </div>
           </div>
           <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-danger"
-              data-bs-dismiss="modal"
-            >
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
               Close
             </button>
-            <button
-              type="submit"
-              class="btn btn-success"
-              data-bs-dismiss="modal"
-            >
+            <button type="submit" class="btn btn-success" data-bs-dismiss="modal">
               Filtra
             </button>
           </div>
