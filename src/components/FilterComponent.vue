83332b14c06e07365c82455acf4f1c1c24c5c9f7
Rachele-Turtora <racheleturtora21@gmail.com>
2024-09-19 14:09:51 +0200

    [FIX] filter distance

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -28,57 +28,65 @@ export default {
       });
     },
     filterResult() {
-      this.store.api.filteredApartments = store.api.filteredApartments;
-
-      // 1. Filtro per  distanza
       if (this.filters.rangeDistance) {
-        this.store.api.filteredApartments =
-          this.store.api.filteredApartments.filter(
-            (apartment) => apartment.distance <= this.filters.rangeDistance
-          );
+        this.getDistance().then(() => {
+          if (this.selectedServices.length > 0) {
+            console.log("Filtraggio per servizi attivi");
+
+            this.store.api.filteredApartments =
+              this.store.api.filteredApartments.filter((appartment) =>
+                this.selectedServices.every((serviceId) =>
+                  appartment.services.some(
+                    (service) => service.pivot.service_id === serviceId
+                  )
+                )
+              );
+          }
+
+          if (this.filters.numrooms > 0) {
+            console.log("filtro camere attivo");
+
+            this.store.api.filteredApartments =
+              this.store.api.filteredApartments.filter(
+                (appartment) => appartment.rooms === this.filters.numrooms
+              );
+          }
+
+          if (this.filters.numbeds > 0) {
+            console.log("filtro letti attivo");
+
+            this.store.api.filteredApartments =
+              this.store.api.filteredApartments.filter(
+                (appartment) => appartment.beds === this.filters.numbeds
+              );
+          }
+
+          if (this.filters.numtoilets > 0) {
+            console.log("filtro bagni attivo");
+
+            this.store.api.filteredApartments =
+              this.store.api.filteredApartments.filter(
+                (appartment) => appartment.bathrooms === this.filters.numtoilets
+              );
+          }
+
+          console.log(this.store.api.filteredApartments);
+          this.$emit("filter-apartments", this.store.api.filteredApartments);
+        });
       }
-      if (this.selectedServices.length > 0) {
-        console.log("Filtraggio per servizi attivi");
+    },
+    getDistance() {
+      const url = this.store.api.baseUrl + this.store.api.endpoints.filteredApartmentsList;
 
-        this.store.api.filteredApartments =
-          this.store.api.filteredApartments.filter((appartment) =>
-            this.selectedServices.every((serviceId) =>
-              appartment.services.some(
-                (service) => service.pivot.service_id === serviceId
-              )
-            )
-          );
+      const params = {
+        city: encodeURIComponent(this.store.search),
+        distance: this.filters.rangeDistance
       }
 
-      if (this.filters.numrooms > 0) {
-        console.log("filtro camere attivo");
-
-        this.store.api.filteredApartments =
-          this.store.api.filteredApartments.filter(
-            (appartment) => appartment.rooms === this.filters.numrooms
-          );
-      }
-
-      if (this.filters.numbeds > 0) {
-        console.log("filtro letti attivo");
-
-        this.store.api.filteredApartments =
-          this.store.api.filteredApartments.filter(
-            (appartment) => appartment.beds === this.filters.numbeds
-          );
-      }
-
-      if (this.filters.numtoilets > 0) {
-        console.log("filtro bagni attivo");
-
-        this.store.api.filteredApartments =
-          this.store.api.filteredApartments.filter(
-            (appartment) => appartment.bathrooms === this.filters.numtoilets
-          );
-      }
-
-      console.log(this.store.api.filteredApartments);
-      this.$emit("filter-apartments", store.api.filteredApartments);
+      return axios.get(url, { params })
+        .then(response => {
+          this.store.api.filteredApartments = response.data.results;
+        });
     },
     close() {
       this.filters.rangeDistance = 10;
@@ -88,12 +96,12 @@ export default {
       this.selectedServices = [];
 
       this.$nextTick(() => {
-        const rangeInput = document.getElementById("range-distance");
+        const rangeInput = document.getElementById('range-distance');
         if (rangeInput) {
           rangeInput.value = 10;
         }
       });
-    },
+    }
   },
   created() {
     this.getServices();
@@ -102,12 +110,7 @@ export default {
 </script>
 <template>
   <!-- Button trigger modal -->
-  <button
-    type="button"
-    class="btn-filters"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-  >
+  <button type="button" class="btn-filters" data-bs-toggle="modal" data-bs-target="#exampleModal">
     <div class="d-flex align-items-center">
       <span>
         <font-awesome-icon :icon="['fas', 'bars-staggered']" class="fs-4" />
@@ -117,65 +120,27 @@ export default {
   </button>
 
   <!-- Modal -->
-  <div
-    class="modal fade"
-    id="exampleModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="exampleModalLabel">
             Filtra la ricerca
           </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-            @click="close()"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="close()"></button>
         </div>
         <form @submit.prevent="filterResult">
           <div class="modal-body">
-            <label for="customRange3" class="form-label"
-              >Cerca nel raggio di:</label
-            >
-            <input
-              type="range"
-              class="form-range"
-              min="5"
-              max="100"
-              step="5"
-              id="range-distance"
-              v-model="filters.rangeDistance"
-            />
-            <span id="range-value" class="d-flex justify-content-center"
-              >{{ filters.rangeDistance }} Km</span
-            >
+            <label for="customRange3" class="form-label">Cerca nel raggio di:</label>
+            <input type="range" class="form-range" min="5" max="100" step="5" id="range-distance"
+              v-model="filters.rangeDistance" />
+            <span id="range-value" class="d-flex justify-content-center">{{ filters.rangeDistance }} Km</span>
 
-            <div
-              class="service-section my-4 d-flex justify-content-evenly flex-wrap"
-            >
-              <div
-                class="service"
-                v-for="service in services"
-                :key="service.id"
-              >
-                <input
-                  type="checkbox"
-                  class="btn-check"
-                  :id="'service-' + service.id"
-                  v-model="this.selectedServices"
-                  :value="service.id"
-                  autocomplete="off"
-                />
-                <label
-                  class="btn btn-outline-primary"
-                  :for="'service-' + service.id"
-                >
+            <div class="service-section my-4 d-flex justify-content-evenly flex-wrap">
+              <div class="service" v-for="service in services" :key="service.id">
+                <input type="checkbox" class="btn-check" :id="'service-' + service.id" v-model="this.selectedServices"
+                  :value="service.id" autocomplete="off" />
+                <label class="btn btn-outline-primary" :for="'service-' + service.id">
                   {{ service.name }}
                 </label>
               </div>
@@ -184,49 +149,26 @@ export default {
             <div class="room-section">
               <div class="row my-2">
                 <h4 class="w-50">Numero di camere</h4>
-                <input
-                  class="input-filter w-25"
-                  type="number"
-                  name="rooms"
-                  id="rooms-numb"
-                  v-model="this.filters.numrooms"
-                />
+                <input class="input-filter w-25" type="number" name="rooms" id="rooms-numb"
+                  v-model="this.filters.numrooms" />
               </div>
               <div class="row my-2">
                 <h4 class="w-50">Numero di letti</h4>
-                <input
-                  class="input-filter w-25"
-                  type="number"
-                  name="beds"
-                  id="beds-numb"
-                  v-model="this.filters.numbeds"
-                />
+                <input class="input-filter w-25" type="number" name="beds" id="beds-numb"
+                  v-model="this.filters.numbeds" />
               </div>
               <div class="row my-2">
                 <h4 class="w-50">Numero di bagni</h4>
-                <input
-                  class="input-filter w-25"
-                  type="number"
-                  name="toilet"
-                  id="toilet-numb"
-                  v-model="this.filters.numtoilets"
-                />
+                <input class="input-filter w-25" type="number" name="toilet" id="toilet-numb"
+                  v-model="this.filters.numtoilets" />
               </div>
             </div>
           </div>
           <div class="modal-footer">
-            <button
-              class="btn btn-danger"
-              data-bs-dismiss="modal"
-              @click="close()"
-            >
+            <button class="btn btn-danger" data-bs-dismiss="modal" @click="close()">
               Reset
             </button>
-            <button
-              type="submit"
-              class="btn btn-primary"
-              data-bs-dismiss="modal"
-            >
+            <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">
               Filtra
             </button>
           </div>
@@ -236,7 +178,7 @@ export default {
   </div>
 </template>
 <style lang="scss">
-@use "../assets/scss/partials/extende" as *;
+@use '../assets/scss/partials/extende' as *;
 
 .btn-filters {
   border: 1px solid $primary;
